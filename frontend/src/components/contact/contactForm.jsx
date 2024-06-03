5c7d4abc6a1abd664bd47e3ce9395dd738a11d47
AticeRuan <104746446+AticeRuan@users.noreply.github.com>
2024-06-04 10:46:00 +1200

    implemented contact page

---
+++ frontend/src/components/contact/contactForm.jsx
@@ -0,0 +1,94 @@
+import { useState } from 'react'
+
+const ContactForm = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: '',
+  })
+
+  const handleChange = (e) => {
+    const { name, value } = e.target
+    setFormData({
+      ...formData,
+      [name]: value,
+    })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    console.log('Form submitted:', formData)
+  }
+
+  return (
+    <div className="w-full  mx-auto ">
+      <form
+        onSubmit={handleSubmit}
+        className="bg-white  rounded px-8 pt-6 pb-8 mb-4 gap-4 flex flex-col"
+      >
+        <div className="mb-4">
+          <label
+            htmlFor="name"
+            className="block text-gray-700  font-bold mb-2 font-heading "
+          >
+            Name
+          </label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            placeholder="What is your name?"
+          />
+        </div>
+        <div className="mb-4">
+          <label
+            htmlFor="email"
+            className="block text-gray-700 font-bold mb-2 font-heading"
+          >
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            className=" appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline "
+            placeholder="How can I reach you?"
+          />
+        </div>
+        <div className="mb-6">
+          <label
+            htmlFor="message"
+            className="block text-gray-700  font-bold mb-2 font-heading"
+          >
+            Message
+          </label>
+          <textarea
+            id="message"
+            name="message"
+            value={formData.message}
+            onChange={handleChange}
+            className=" appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            placeholder="What can I help you with?"
+            rows={10}
+          ></textarea>
+        </div>
+        <div className="flex items-center justify-between">
+          <button
+            type="submit"
+            className="bg-jinsook-green hover:bg-white text-white hover:text-jinsook-green font-body font-[700] py-2 px-4 rounded focus:outline-none focus:shadow-outline hover:border-2 border-jinsook-green transition duration-500 ease-in-out h-[50px] w-[100px] uppercase text-center"
+          >
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default ContactForm
--- frontend/src/pages/contact.jsx
+++ frontend/src/pages/contact.jsx
@@ -1,7 +1,30 @@
+import ContactForm from '../components/contact/contactForm'
+import PageTitle from '../components/ui/pageTitle'
+
 const Contact = () => {
   return (
-    <div className="w-screen xl:w-[1000px] min-h-[calc(100vh-120px)] pt-[px] md:pt-[150px] flex flex-col items-center">
-      Contact
+    <div className="w-screen xl:w-[1000px] min-h-[calc(100vh-120px)] pt-[px] md:pt-[150px] flex flex-col items-center gap-28">
+      {/* heading */}
+      <div className="text-start w-full mt-10">
+        <PageTitle heading="Contact Jinsook " />
+      </div>
+      <div className="flex flex-col lg:flex-row w-full px-20 items-start gap-10 xl:px-5  ">
+        {/* blurb */}
+        <div className="flex flex-col gap-12 md:gap-20 items-start justify-center lg:w-1/2 ">
+          <p className="font-heading font-[700] text-[2rem]">
+            Let&apos;s work togther
+          </p>
+          <p className="font-body text-[1rem] text-justify">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
+            dignissim enim et eros porttitor, vitae elementum arcu placerat.
+            Nunc porttitor urna scelerisque risus fermentum pharetra.
+          </p>
+        </div>
+        {/* form */}
+        <div className="lg:w-1/2 w-full">
+          <ContactForm />
+        </div>
+      </div>
     </div>
   )
 }
