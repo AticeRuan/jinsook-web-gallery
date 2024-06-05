8d95ea0209fe9e768ec55e1ffcfa9b0f64f414ec
AticeRuan <104746446+AticeRuan@users.noreply.github.com>
2024-06-05 12:28:42 +1200

    deployment testing

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express')
-const artworkRoutes = require('../routes/artworks')
-const userRoutes = require('../routes/user')
+const artworkRoutes = require('./routes/artworks')
+const userRoutes = require('./routes/user')
 const mongoose = require('mongoose')
 const cors = require('cors')
 
